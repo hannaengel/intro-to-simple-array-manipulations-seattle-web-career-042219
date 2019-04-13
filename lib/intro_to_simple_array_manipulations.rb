d5b43100ccd8f867007fe9905e4a68db1dc92a72
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-13 20:23:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(firstArray, secondArray)
 end
 
 def using_insert(array, element)
-  array.insert(3, element)
+  array.insert(4, element)
 end
 
 def using_uniq(array)
@@ -38,6 +38,6 @@ def using_flatten(array)
   array.flatten
 end
 
-def using_delete(array, string)
+def using_delete_at(array, string)
   array.delete(string)
 end
